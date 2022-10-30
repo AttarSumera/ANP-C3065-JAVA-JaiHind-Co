dbb946472a656dc93c19591d5f344fc505fa33c8
HOME <HOME@DESKTOP-IDJ0LAQ>
2022-10-30 11:20:17 +0530

    version 2

---
+++ ifdemo/src/ifdemo.java
@@ -0,0 +1,17 @@
+
+public class ifdemo {
+
+	public class void main(String[] args) {
+		int age=90;
+		if(age>=18)
+			System.out.println(Eligible for voting);
+			
+					if(age>=18)
+					{
+						System.out.println("Eligible for voting");
+								System.out.println("end of if statement");
+											
+		}
+					System.out.println("Byee!!!");
+	}
+}
