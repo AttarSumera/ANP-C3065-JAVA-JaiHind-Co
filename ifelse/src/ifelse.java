298940508852ced9ea3bc306ba3e22f279f4799b
HOME <HOME@DESKTOP-IDJ0LAQ>
2022-10-30 11:20:51 +0530

    version 2

---
+++ ifelse/src/ifelse.java
@@ -0,0 +1,19 @@
+
+public class ifelse {
+
+	public static void main(String[] args)
+	{
+		int age=23;
+		if(age>=18)
+		{
+			System.out.println("if block");
+			System.out.println("eligible for voting");
+		}
+		else {
+			System.out.println("else block");
+			System.out.println("not eligible for voting");
+		}
+		int sum=5+8;
+		System.out.println("byee!!");
+	}
+}
