f79c2495c73e4ab05a6c13cba71741a70bfccf0f
HOME <HOME@DESKTOP-IDJ0LAQ>
2022-10-30 11:19:41 +0530

    version 2

---
+++ Addition/src/Addition.java
@@ -0,0 +1,11 @@
+
+public class Addition {
+	public static void main(String args[])
+	{
+		int num1=90;
+		int num2=89;
+		int result=num1+num2;
+		System.out.println("The result is"+result);
+	}
+
+}
